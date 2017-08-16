3dbf585c80aa1fbecd2ebffb234a95189b78942c
selvaganeshr <selvaganeshrajam@gmail.com>
2017-08-16 09:08:47 +0530

    config updated

--- src/main/java/com/titan/framework/RestAssuredConfiguration.java
+++ src/main/java/com/titan/framework/RestAssuredConfiguration.java
@@ -2,7 +2,14 @@ package com.titan.framework;
 
 import io.restassured.RestAssured;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
+
+
+import static io.restassured.RestAssured.given;
 
 
 /**
@@ -14,5 +21,19 @@ public class RestAssuredConfiguration {
     public void config(){
         RestAssured.baseURI = "https://api.github.com/";
     }
+
+
+    public RequestSpecification getRequestSpecification(){
+        return given().contentType(ContentType.JSON);
+    }
+
+
+    public Response getResponse(RequestSpecification requestSpecification,String endpoint, int status){
+        Response response = requestSpecification.get(endpoint);
+        Assert.assertEquals(response.getStatusCode(),status);
+        response.then().log().all();
+        return response;
+    }
+
 
 }
